b04243c772778732b7980b52f6ebaa1d7173ac40
Sofia <savth2004@gmail.com>
2024-03-09 19:35:47 +0600

    create toml file and change dependency installation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,25 +51,25 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    //Coil
-    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation(libs.coreKtx)
+    implementation(libs.lifecycleKtx)
+    implementation(platform(libs.composeBom))
+    implementation(libs.composeActivity)
+    implementation(libs.composeRuntime)
+    implementation(libs.composeUi)
+    implementation(libs.composeGraphics)
+    implementation(libs.composePreview)
+    implementation(libs.composeDebugPreview)
+    implementation(libs.composeDebugManifest)
+    implementation(libs.composeMaterial)
+    implementation(libs.composeMaterial3)
+    implementation(libs.jUnit)
+    implementation(libs.jUnitTest)
+    implementation(libs.jUnitUi)
+    implementation(libs.espressoTest)
 
-    //Navigation
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(libs.coil)
+    implementation(libs.navigation)
+
 }
